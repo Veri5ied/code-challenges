86f42e8be21cfb28a10c29af1846905966409ccb
Veri5ied <alvinuchenna14@gmail.com>
2022-11-08 08:51:20 +0100

    manual

--- string-reverse/index.js
+++ string-reverse/index.js
@@ -2,6 +2,15 @@ function stringReverse(str) {
   return str.split("").reverse().join("");
 }
 
+//manual solution
+function stringReverse(str) {
+  let reversed = "";
+  for (let constant of str) {
+    reversed += constant;
+  }
+  return reversed;
+}
+
 console.log(stringReverse("hello"));
 
 module.exports = stringReverse;
