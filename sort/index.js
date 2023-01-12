532731a4ad4de9d51506ea10687b3a796962145e
Alvin Okoro <43749581+Veri5ied@users.noreply.github.com>
2023-01-12 11:20:22 +0100

    sorting

---
+++ sort/index.js
@@ -0,0 +1,3 @@
+const users = ["user f", "user g", "user a", "user c", "user j"];
+const sortedUsers = users.sort();
+console.log(sortedUsers);
---
+++ sort/sort.js
@@ -0,0 +1,4 @@
+const mynums = [10, 3, 4, 2, 1, 9, 101, 22]
+
+const sortednums = mynums.sort((a, b) => a - b)
+console.log(sortednums)
